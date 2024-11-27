596a952e419debe063b1877806262f38a0a36f10
victoriarahme <vic.rahme@gmail.com>
2024-11-26 21:07:53 -0500

    JS and json added, part 2 complete

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+//PART 1:
 let student = {
     name: "Victoria",
     age: 26,
+    //boolean
     enrolled: true,
- //course array   
-course: ["Algorithms", "Web design", "Javascript"],
-information: function () {
-console.log(student.name, student.age, student.enrolled, student.course);
+    //course array   
+    course: ["Algorithms", "Web design", "Javascript"],
+    //method
+    information: function () {
+        
+        console.log(student.name, student.age, student.enrolled, student.course);
 }};
-console.log(student.name, student.age);
+        console.log(student.name, student.age);
 
-student.information();
+        student.information();
+//PART 2:
+        let studentJson = JSON.stringify(student);
+
+        console.log(studentJson);
